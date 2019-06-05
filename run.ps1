d9a903f15ba4cfc29c73fd696a5c21b39944ec9f
wangandi <wangandi@gridsum.com>
2019-06-05 14:42:20 +0800

    Update run.ps1.

--- run.ps1
+++ run.ps1
@@ -4,5 +4,17 @@ $secpasswd = ConvertTo-SecureString $password -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("9ea3912e-4bb8-4096-a76a-7e04076a4674", $secpasswd)
 Add-AzureRmAccount -ServicePrincipal -Tenant '504b54d2-efc8-4dc3-b5ab-c15111f0b72e' -Credential $mycreds
 Select-AzureRmSubscription -SubscriptionId 'b124cd91-de72-4792-9855-70a5858da9bd'
+Stop-AzureRmWebApp -Name 'andymeng' -ResourceGroupName 'Mine'
+Start-AzureRmWebApp -Name 'andymeng' -ResourceGroupName 'Mine'
+Stop-AzureRmWebApp -Name 'ZhuoYao' -ResourceGroupName 'Mine'
+Start-AzureRmWebApp -Name 'ZhuoYao' -ResourceGroupName 'Mine'
+Stop-AzureRmWebApp -Name 'ZhuoYao1' -ResourceGroupName 'Mine'
+Start-AzureRmWebApp -Name 'ZhuoYao1' -ResourceGroupName 'Mine'
+Stop-AzureRmWebApp -Name 'ZhuoYaoSpider' -ResourceGroupName 'Mine'
+Start-AzureRmWebApp -Name 'ZhuoYaoSpider' -ResourceGroupName 'Mine'
+Stop-AzureRmWebApp -Name 'ZhuoYaoSpider1' -ResourceGroupName 'ZhuoYao'
+Start-AzureRmWebApp -Name 'ZhuoYaoSpider1' -ResourceGroupName 'ZhuoYao'
+Stop-AzureRmWebApp -Name 'ZhuoYaoSpider2' -ResourceGroupName 'ZhuoYao'
+Start-AzureRmWebApp -Name 'ZhuoYaoSpider2' -ResourceGroupName 'ZhuoYao'
 Stop-AzureRmWebApp -Name 'ZhuoYaoSpider3' -ResourceGroupName 'ZhuoYao'
 Start-AzureRmWebApp -Name 'ZhuoYaoSpider3' -ResourceGroupName 'ZhuoYao'
